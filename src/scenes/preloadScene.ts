2a2e8657b26cc8a9442dc72f9a051ffe5801b080
John Henry Cooper <122650638+jhcooper@users.noreply.github.com>
2024-02-22 15:17:15 -0500

    added sky and platforms

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -6,7 +6,18 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("phaser-logo", "assets/img/phaser-logo.png");
+        this.load.image("sky", "/assets/phaser3_tutorial_assets/sky.png");
+        this.load.image(
+            "ground",
+            "/assets/phaser3_tutorial_assets/platform.png"
+        );
+        this.load.image("star", "/assets/phaser3_tutorial_assets/star.png");
+        this.load.image("bomb", "/assets/phaser3_tutorial_assets/bomb.png");
+        this.load.spritesheet(
+            "dude",
+            "/assets/phaser3_tutorial_assets/dude.png",
+            { frameWidth: 32, frameHeight: 48 }
+        );
     }
 
     create() {
