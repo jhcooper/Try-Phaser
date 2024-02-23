641985038d8a7161ac20fd47314ebb8adcb91afc
John Henry Cooper <122650638+jhcooper@users.noreply.github.com>
2024-02-23 17:39:53 -0500

    extracting assets

--- src/scenes/preloadScene.ts
+++ src/scenes/preloadScene.ts
@@ -6,18 +6,14 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("sky", "/assets/phaser3_tutorial_assets/sky.png");
-        this.load.image(
-            "ground",
-            "/assets/phaser3_tutorial_assets/platform.png"
-        );
-        this.load.image("star", "/assets/phaser3_tutorial_assets/star.png");
-        this.load.image("bomb", "/assets/phaser3_tutorial_assets/bomb.png");
-        this.load.spritesheet(
-            "dude",
-            "/assets/phaser3_tutorial_assets/dude.png",
-            { frameWidth: 32, frameHeight: 48 }
-        );
+        this.load.image("sky", "/assets/sky.png");
+        this.load.image("ground", "/assets/platform.png");
+        this.load.image("star", "/assets/star.png");
+        this.load.image("bomb", "/assets/bomb.png");
+        this.load.spritesheet("dude", "/assets/dude.png", {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
     }
 
     create() {
